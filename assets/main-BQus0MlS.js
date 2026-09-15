eb85b3228164864cd3b4f73869501198e98a47a3
kosheklov <127956655+kosheklov@users.noreply.github.com>
2026-09-15 18:29:11 +0300

    Add compiled site assets

---
+++ assets/main-BQus0MlS.js
@@ -0,0 +1,1 @@
+import{c as t,j as e,R as o,A as r}from"./styles-D1UU1EMX.js";t(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
---
+++ assets/zplatka-C6TEI4Y3.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as r,A as a}from"./styles-D1UU1EMX.js";e(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(a,{zplatka:!0,compactDefault:!0})}));
